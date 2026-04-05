4091dccef2104b2e75d488d159cfa9bedf61a77a
Joenstalker <2301107552@student.buksu.edu.ph>
2026-04-06 00:45:24 +0800

    feat: fixing the tenant db

--- app/Models/SystemEarning.php
+++ app/Models/SystemEarning.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SystemEarning extends Model
 {
+    protected $connection = 'mysql';
+
     protected $fillable = [
         'tenant_id',
         'amount',
