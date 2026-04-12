92676864a578bb9359ff70e35daed72fac79f0f9
Joenstalker <2301107552@student.buksu.edu.ph>
2026-04-12 21:49:33 +0800

    feat: secure the central auth

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -10,13 +10,22 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
+        $email = config('admin_seed.email');
+        $password = config('admin_seed.password');
+
+        if (blank($email) || blank($password)) {
+            $this->command?->warn('AdminUserSeeder skipped: set ADMIN_SEED_EMAIL and ADMIN_SEED_PASSWORD in .env');
+
+            return;
+        }
+
         User::updateOrCreate(
-        ['email' => 'dcmsapps@gmail.com'],
-        [
-            'name' => 'DCMS Admin',
-            'password' => Hash::make('password'),
-            'is_admin' => true,
-        ]
+            ['email' => $email],
+            [
+                'name' => config('admin_seed.name'),
+                'password' => Hash::make($password),
+                'is_admin' => true,
+            ]
         );
     }
 }
