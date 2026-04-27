1fb3767e0f8ff7cca3ea4b24c274c0f0087b9ebd
ChristianPrincipe <2301104738@student.buksu.edu.ph>
2026-04-28 00:57:02 +0800

    feat: add dental release mfsc

---
+++ app/Models/DentalRelease.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DentalRelease extends Model
+{
+    /** @use HasFactory<\Database\Factories\DentalReleaseFactory> */
+    use HasFactory;
+}
